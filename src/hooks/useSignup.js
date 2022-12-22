47713a432fd9c98b09a7236bfcccb1cbe00a91d8
devcys22 <ysoli36512@gmail.com>
2022-12-22 17:46:17 +0900

    context로 유저정보 관리하기

--- src/hooks/useSignup.js
+++ src/hooks/useSignup.js
@@ -1,12 +1,15 @@
 import {useState} from "react"
 import {appAuth} from '../firebase/config'
 import { createUserWithEmailAndPassword, updateProfile} from "firebase/auth";
+import { useAuthContext } from "./useAuthContext";
+import { dispatch } from './useAuthContext';
 
 export const useSignup = () => {
     //에러 정보를 저장합니다.
     const [error, setError] = useState(null);
     // 현재 서버와 통신 상태를 저장합니다.
     const [isPending, setIsPending] = useState(false);
+    const {dispatch} = useAuthContext();
 
     const signup = ( email, password, displayName ) => {
         setError(null); //아직 에러가 없습니다.
@@ -23,6 +26,7 @@ export const useSignup = () => {
 
                 updateProfile(appAuth.currentUser, {displayName})
                     .then(() => {
+                        dispatch({ type: 'login', payload: user});
                         setError(null);
                         setIsPending(false);
                     }).catch((err) => {

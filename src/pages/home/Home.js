1b6e660215d5f770e2e8edb868803463780e5ede
devcys22 <ysoli36512@gmail.com>
2022-12-23 16:53:48 +0900

    상태에 맞는 화면 & 홈 화면 만들기

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,7 +1,15 @@
+import DiaryForm from './DiaryForm'
 import styles from './Home.module.css'
 
 export default function Home(){
     return(
-        <div>Home</div>
+        <main className={styles.cont}>
+            <aside className={styles.side_menu}>
+                <DiaryForm></DiaryForm>
+            </aside>
+            <ul className={styles.content_list}>
+                dairy list
+            </ul>
+        </main>
     )
 }
